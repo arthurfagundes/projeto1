9f6a429db2758438c97d1cec87ea2786a39b305f
arthurfagundes <64553764+arthurfagundes@users.noreply.github.com>
2023-11-03 22:09:03 -0300

    initial commit

---
+++ exercicio4.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!-- Desenvolva em PHP um algoritmo que: 
+Um restuarante vende comida a quilo, ela cobra r$21,50 por quilo de refeição. O usuario entra com o peso do prato montado pelo usuario
+não esqueça a tara do prato que pesa 200g). Mostre na tela o valor que o usuario deverá pagar. -->
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 4 - Restaurante</title>
+</head>
+<body>
+    
+    
+</body>
+</html>
---
+++ exercicio5.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!-- Crie um algoritmo em php onde é informado o nome de 2 time(ex: GRÊMIO e inter) e o
+número de gols marcados na partida ex:(GRÊMIO - 5 gold e inter - 0 gols)
+Mostre na tela o nome do time que ganhou a partida e a quantidade de gols: Ex:(GRÊMIO VENCEU A PARTIDA 5X0inter) -->
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 4 - Restaurante</title>
+</head>
+<body>
+    
+    
+</body>
+</html>
